b296b980035147be5cb5310a141c2bd55154417d
monirulislamweb <65311288+monirulislamweb@users.noreply.github.com>
2021-11-07 21:51:32 +0600

    ..

--- themes/terrav/assets/js/main.js
+++ themes/terrav/assets/js/main.js
@@ -277,10 +277,11 @@ if( $('#customSidebar').length ){
                 var boxh = $('#customSidebar .vacature-details-con-rgt-inner').height();
                 var ftrtop = $(".footer-wrp").offset().top;
                 //var RelevantTop = $(".user-rel-camp-sec").offset().top;
-                var ftrx = (ftrtop - boxh) - 450;
+                var ftrx = (ftrtop - boxh) - 145;
                 //var RelevantTrx = (RelevantTop - boxh) - 160;
                 if( scroll < ftrx ){
-                    $('#customSidebar').css('top', scroll);
+                    var fullshow = (scroll -300);
+                    $('#customSidebar').css('top', fullshow);
                 }
                /* if( scroll < RelevantTrx ){
                     $('#customSidebar').css('top', scroll);
