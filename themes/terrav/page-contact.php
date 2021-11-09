8592a59b9cafdd6bd8da064811e0863f498df424
Azizul Haque <azizul.softsw@gmail.com>
2021-11-09 21:28:30 +0600

    contact/404

---
+++ themes/terrav/page-contact.php
@@ -0,0 +1,100 @@
+<?php
+/*Template Name: Contact*/
+get_header();
+$thisID = get_the_ID();
+$intro = get_field('introsec', $thisID);
+$mapcode = $intro['map_code'];
+$telefoon = get_field('telefoon', 'options');
+$email = get_field('emailadres', 'options');
+$fax = get_field('fax', 'options');
+$adres = get_field('adres', 'options');
+$map_url = get_field('map_url', 'options');
+$gmaplink = !empty($map_url)?$map_url:'javascript:void()';
+?>
+<section class="page-banner">
+  <span class="pg-bnr-rgt-img hide-sm">
+    <img src="<?php echo THEME_URI; ?>/assets/images/page-bnr-right-img.svg" alt="">
+  </span>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="page-banner-cntlr">
+          <h1 class="fl-h1 page-banner-title">Contact</h1>
+          <div class="breadcrumb-cntlr hide-sm">
+            <ul class="reset-list clearfix">
+              <li class="home">
+                <a href="#">
+                  <span class="item">Home</span>
+                </a>
+              </li>
+              <li>
+                <a href="#"><span>Binnenpagina</span></a>
+              </li>
+              <li class="active">
+                <span>Binnenpagina</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="contact-form-sec-wrp">
+<div class="container">
+  <div class="row">
+    <div class="col-md-12">
+      <div class="contact-form-block clearfix">
+        <div class="contact-form-lft">
+          <div class="contact-form-info-cntlr">
+            <div class="contact-form-info">
+              <div class="contact-info-details-cntlr">
+                <div class="cnt-info-details-inr">
+                  <h3 class="contact-form-info-title fl-h2"><?php _e('Contact Info', 'terrav'); ?></h3>
+                  <div class="contact-details">
+			              <?php 
+		                if( !empty($adres) ) printf('<div class="cnt-addrs cnt-details-col"><span class="cnt-addrs-title">%s</span><a href="tel:%s">%s</a></div>', __('Magazijn 50-52', 'terrav'), $gmaplink,  $adres); 
+		                if( !empty($telefoon) ) printf('<div class="cnt-tel cnt-details-col"><span>%s : <a href="tel:%s">%s</a></span></div>', __('Tel.', 'terrav'), phone_preg($telefoon),  $telefoon); 
+		                if( !empty($fax) ) printf('<div class="cnt-fax cnt-details-col"><span>%s : <a href="fax:%s">%s</a></span></div>', __('Fax', 'terrav'), phone_preg($fax),  $fax); 
+		                if( !empty($email) ) printf(' <div class="cnt-mail cnt-details-col"><span>%s : <a href="mailto:%s">%s</a></span></div> ', __('E-mail', 'terrav'), $email, $email);  
+			              ?>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="contact-form-rgt">
+          <div class="contact-forn-con-cntlr">
+            <div class="contact-form-dsc-wrp">
+              <div class="contact-entry-header">
+                <?php 
+                	if( !empty($intro['titel']) ) printf( '<h2 class="cnt-sec-entry-hdr-title fl-h2 hide-sm">%s</h2>', $intro['titel'] ); 
+                	if( !empty($intro['beschrijving']) ) echo wpautop( $intro['beschrijving'] );
+                ?>
+              </div>
+            </div>
+            <div class="contact-form-wrp clearfix">
+              <div class="wpforms-container">
+              	<?php if( !empty($intro['shortcode']) ) echo do_shortcode($intro['shortcode']); ?>
+              </div>
+            </div>
+          </div>   
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</section>
+
+<?php if( !empty($mapcode) ): ?>
+<section class="contact-google-map-sec-wrp">
+<div class="contact-google-map-wrp">
+  <div class="contact-google-map">
+    <?php printf('%s', $mapcode); ?>
+  </div> 
+</div>
+</section>
+<?php endif; ?>
+<?php get_footer(); ?>
--- themes/terrav/templates/cta-sec.php
+++ themes/terrav/templates/cta-sec.php
@@ -1,3 +1,7 @@
+<?php 
+  $cta = get_field('cta', 'options'); 
+  if( $cta ):
+?>
 <section class="footer-top-cta-sec">
   <div class="container">
     <div class="row">
@@ -6,20 +10,24 @@
           <div class="footer-top-cta-desc-cntlr">
             <div class="desc-entry-sun-hdr">
               <h3 class="fl-h6 footer-top-cta-desc-sub-title entry-sub-title">
-                <span>Contact</span>
-                <i><img src="assets/images/white-2lines.svg" alt=""></i>
+                <?php if( !empty($cta['top_titel']) ) printf('<span>%s</span>', $cta['top_titel']); ?>
+                <i><img src="<?php echo THEME_URI; ?>/assets/images/white-2lines.svg" alt=""></i>
               </h3>
             </div>
-            <h2 class="fl-h1 footer-top-cta-title">Facilisi duis aliquet.</h2>
+            <?php if( !empty($cta['titel']) ) printf('<h2 class="fl-h1 footer-top-cta-title">%s</h2>', $cta['titel']); ?>
             <div class="footer-top-cta-desc">
-              <p>Blandit volutpat enim rutrum maecenas nunc, consectetur nec a commodo. Pulvinar erat sed a tempus. Sed et purus, eget viverra a.</p>
+              <?php if( !empty($cta['beschrijving']) ) echo wpautop($cta['beschrijving']); ?>
             </div>
           </div>
-          <div class="footer-top-cta-btn">
-            <a class="fl-btn" href="#">contacteer ons</a>
-          </div>
+          <?php 
+            $contlink = $cta['knop'];
+            if( is_array( $contlink ) &&  !empty( $contlink['url'] ) ){
+                printf('<div class="footer-top-cta-btn"><a class="fl-btn" href="%s" target="%s">%s</a></div>', $contlink['url'], $contlink['target'], $contlink['title']); 
+            }
+          ?>
         </div>
       </div>
     </div>
   </div>
 </section>
+<?php endif; ?>
