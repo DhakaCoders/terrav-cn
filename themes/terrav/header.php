a53370592955cd85f56d3df413a1e3e778fe037e
Azizul Haque <azizul.softsw@gmail.com>
2021-11-09 18:43:56 +0600

    hd/ft

---
+++ themes/terrav/header.php
@@ -0,0 +1,266 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>> 
+<head>
+<meta charset="<?php bloginfo('charset'); ?>">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="theme-color" content="#EE3E15">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<?php $favicon = get_theme_mod('favicon'); if(!empty($favicon)) { ?> 
+<link rel="shortcut icon" href="<?php echo $favicon; ?>" />
+<?php } ?>
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+<svg style="display: none;">
+  <!-- start of Noyon -->
+
+  <symbol id="msg-tel-icon" width="24" height="24" viewBox="0 0 24 24"  xmlns="http://www.w3.org/2000/svg">
+    <path d="M20.1551 10.2309C19.9402 10.0263 19.6548 9.91223 19.3581 9.91233C19.0614 9.91243 18.7761 10.0267 18.5613 10.2315L17.2377 11.5583L12.4672 6.78434L13.7913 5.46012C14.0022 5.24872 14.1207 4.96227 14.1207 4.66362C14.1207 4.36497 14.0022 4.07851 13.7913 3.86712L10.3385 0.414277C10.1258 0.207506 9.84051 0.0923746 9.54389 0.0936072C9.24728 0.0948398 8.96297 0.212338 8.75203 0.42087C6.15583 2.55108 5.77696 6.49077 8.21205 8.89518L9.0018 9.68493H1.57227C1.17458 9.68538 0.793314 9.84356 0.512107 10.1248C0.230901 10.406 0.0727202 10.7872 0.0722656 11.1849V19.8099C0.0727202 20.2076 0.230901 20.5889 0.512107 20.8701C0.793314 21.1513 1.17458 21.3095 1.57227 21.3099H9.07666L10.6536 23.3721C10.8088 23.5764 11.0191 23.7322 11.2597 23.8212C11.5004 23.9102 11.7614 23.9288 12.0123 23.8748C12.2631 23.8208 12.4933 23.6963 12.676 23.5161C12.8586 23.3359 12.9861 23.1073 13.0434 22.8572L13.3976 21.3099H14.7011C15.097 21.3081 15.4763 21.1506 15.757 20.8714C16.0377 20.5922 16.1972 20.2137 16.2011 19.8178L16.2187 16.6534C17.0876 17.174 18.0812 17.4496 19.0942 17.4508C20.7911 17.4924 22.4495 16.6 23.6013 15.2702C23.8106 15.0579 23.9279 14.7718 23.9279 14.4737C23.9279 14.1756 23.8106 13.8895 23.6013 13.6772L20.1551 10.2309ZM12.7997 19.8099C12.6299 19.8099 12.4651 19.8676 12.3323 19.9734C12.1995 20.0792 12.1065 20.227 12.0686 20.3926L11.6528 22.2095L10.0431 20.1044C9.97309 20.0128 9.88292 19.9386 9.77958 19.8875C9.67625 19.8365 9.56253 19.8099 9.44726 19.8099H1.57227V11.1849H10.5018L14.7256 15.4087L14.7011 19.8099L12.7997 19.8099ZM22.0008 14.7496C21.2287 15.5181 20.1836 15.9496 19.0942 15.9496C18.0048 15.9496 16.9597 15.5181 16.1875 14.7496L9.27258 7.83463C8.87068 7.42717 8.55733 6.94105 8.35218 6.40677C8.14702 5.87249 8.0545 5.30158 8.08041 4.72985C8.10632 4.15812 8.2501 3.59792 8.50274 3.08438C8.75538 2.57085 9.11142 2.11506 9.54852 1.74563L12.4667 4.6636L11.1478 5.98269C11.0418 6.08662 10.9575 6.21052 10.8998 6.34724C10.842 6.48395 10.8119 6.63076 10.8112 6.77918C10.8106 6.92759 10.8394 7.07467 10.8959 7.21189C10.9524 7.34912 11.0356 7.47377 11.1406 7.57864L16.443 12.885L16.4434 12.8856C16.6545 13.0959 16.9405 13.2139 17.2385 13.2138C17.5365 13.2136 17.8224 13.0953 18.0333 12.8847L19.359 11.5559L22.2767 14.4737L22.0008 14.7496Z" />
+  </symbol>
+
+  <!-- start of Kashob -->
+  <symbol id="green-2lines" width="28" height="31" viewBox="0 0 28 31" xmlns="http://www.w3.org/2000/svg">
+    <rect y="9.21118" width="7.08561" height="24.0911" transform="rotate(-30 0 9.21118)"/>
+    <rect x="9.81836" y="3.54272" width="7.08561" height="24.0911" transform="rotate(-30 9.81836 3.54272)"/>
+  </symbol>
+
+  <symbol id="info-arow" width="8" height="12" viewBox="0 0 8 12" xmlns="http://www.w3.org/2000/svg">
+    <path d="M3.34982 0.563634L7.088 4.96495C7.34102 5.26274 7.46734 5.63129 7.46734 5.99983C7.46734 6.36797 7.34102 6.73611 7.088 7.0347L3.34982 11.436C2.77822 12.1092 1.76932 12.1915 1.0962 11.6199C0.423067 11.0479 0.340725 10.039 0.912724 9.36587L3.77112 5.99983L0.912724 2.63418C0.341124 1.96026 0.423466 0.951762 1.0962 0.380162C1.76932 -0.191438 2.77822 -0.109095 3.34982 0.563634Z"/>
+  </symbol>
+
+  <symbol id="Svic-grid-2line" width="41" height="44" viewBox="0 0 41 44" xmlns="http://www.w3.org/2000/svg">
+    <rect width="10.1484" height="34.5045" transform="matrix(0.866024 -0.500002 0.499998 0.866026 0 13.1929)"/>
+    <rect width="10.1484" height="34.5045" transform="matrix(0.866024 -0.500002 0.499998 0.866026 14.0625 5.07422)"/>
+  </symbol>
+
+  <symbol id="search-icon" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M23.4316 20.5276L17.9139 15.0094C18.8514 13.5169 19.3955 11.7528 19.3955 9.8596C19.3955 4.50348 15.0535 0.162048 9.69755 0.162048C4.34163 0.162048 0 4.50348 0 9.8596C0 15.2159 4.34143 19.5569 9.69755 19.5569C11.7578 19.5569 13.6662 18.9128 15.2365 17.8183L20.6886 23.2708C21.0675 23.6492 21.5641 23.8378 22.0601 23.8378C22.5568 23.8378 23.0528 23.6492 23.4322 23.2708C24.1893 22.5129 24.1893 21.2853 23.4316 20.5276ZM9.69755 16.4151C6.07744 16.4151 3.14247 13.4803 3.14247 9.86C3.14247 6.23969 6.07744 3.30472 9.69755 3.30472C13.3179 3.30472 16.2526 6.23969 16.2526 9.86C16.2526 13.4803 13.3179 16.4151 9.69755 16.4151Z"/>
+  </symbol>
+
+  <!-- start of Shariful -->
+
+
+  <!-- start of Momin -->
+  <symbol id="bnr-arrow-icon" width="13" height="14" viewBox="0 0 13 14"  xmlns="http://www.w3.org/2000/svg">
+    <path d="M5.56003 13.03H5.64463L5.83263 13.2274C5.92001 13.3155 6.02398 13.3854 6.13852 13.4332C6.25307 13.4809 6.37593 13.5055 6.50003 13.5055C6.62412 13.5055 6.74698 13.4809 6.86153 13.4332C6.97608 13.3854 7.08004 13.3155 7.16742 13.2274L7.35543 13.03H7.44003V12.9454L12.8074 7.58742L11.4726 6.25262L7.44003 10.2946V0.340027H5.56003V10.2946L1.52743 6.25262L0.192627 7.58742L5.56003 12.9454V13.03Z" />
+  </symbol>
+
+  <!-- start of Jahir -->
+
+  <!--start of niaz -->
+  <symbol id="equal-icon" width="28" height="31" viewBox="0 0 28 31" xmlns="http://www.w3.org/2000/svg">
+    <rect y="9.21118" width="7.08561" height="24.0911" transform="rotate(-30 0 9.21118)" fill="#4F7F35"/>
+    <rect x="9.81836" y="3.54272" width="7.08561" height="24.0911" transform="rotate(-30 9.81836 3.54272)" fill="#4F7F35"/>
+  </symbol>
+
+
+
+</svg>  
+<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+<div class="page-body-cntlr">
+<div class="bdoverlay"></div>
+<header class="header">
+  <div class="container-lg">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="header-inr clearfix">
+          <div class="hdr-lft">
+            <div class="logo">
+              <a href="#"><img src="<?php echo THEME_URI; ?>/assets/images/logo.png"></a>
+            </div>
+          </div>
+          <div class="hdr-menu hide-lg">
+            <nav class="main-nav">
+              <ul class="clearfix reset-list">
+                <li class="current-menu-item"><a href="#">Producten</a></li>
+                <li class="menu-item-has-children">
+                  <a href="#">Verpakkingen</a>
+                  <ul class="sub-menu">
+                    <li><a href="#">submenu 1</a></li>
+                    <li><a href="#">submenu 2</a></li>
+                    <li><a href="#">submenu 3</a></li>
+                  </ul>
+                </li>
+                <li><a href="#">Over ons</a></li>
+                <li><a href="#">Nieuws</a></li>
+                <li><a href="#">Vacatures</a></li>
+              </ul>
+            </nav>
+          </div>
+          <div class="hdr-right-cntlr hide-lg">
+            <div class="hdr-right">
+              <div class="hdr-lang-cntlr">
+                <div class="hdr-language">
+                  <div class="fl-lang reset-slect">
+                    <div class="fl-lang-cntlr">
+                      <ul class="reset-list">
+                        <li class="lang-active"><a href="#">Nl</a></li>
+                        <li><a href="#">EN</a></li>
+                        <li><a href="#">CN</a></li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="hdr-contact-btn">
+                <a href="#">
+                  <span>Contact</span>
+                  <i>
+                    <svg class="msg-tel-icon" width="24" height="24" viewBox="0 0 24 24" fill="#fff">
+                      <use xlink:href="#msg-tel-icon"></use> 
+                    </svg>
+                  </i>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="hamburgar-cntlr show-lg">
+            <div class="hamburger-icon">
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</header>
+
+<header class="header header-sticky">
+  <div class="container-lg">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="header-inr clearfix">
+          <div class="hdr-lft">
+            <div class="logo">
+              <a href="#"><img src="<?php echo THEME_URI; ?>/assets/images/logo.png"></a>
+            </div>
+          </div>
+          <div class="hdr-menu hide-lg">
+            <nav class="main-nav">
+              <ul class="clearfix reset-list">
+                <li class="current-menu-item"><a href="#">Producten</a></li>
+                <li class="menu-item-has-children">
+                  <a href="#">Verpakkingen</a>
+                  <ul class="sub-menu">
+                    <li><a href="#">submenu 1</a></li>
+                    <li><a href="#">submenu 2</a></li>
+                    <li><a href="#">submenu 3</a></li>
+                  </ul>
+                </li>
+                <li><a href="#">Over ons</a></li>
+                <li><a href="#">Nieuws</a></li>
+                <li><a href="#">Vacatures</a></li>
+              </ul>
+            </nav>
+          </div>
+          <div class="hdr-right-cntlr hide-lg">
+            <div class="hdr-right">
+              <div class="hdr-lang-cntlr">
+                <div class="hdr-language">
+                  <div class="fl-lang reset-slect">
+                    <div class="fl-lang-cntlr">
+                      <ul class="reset-list">
+                        <li class="lang-active"><a href="#">Nl</a></li>
+                        <li><a href="#">EN</a></li>
+                        <li><a href="#">CN</a></li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="hdr-contact-btn">
+                <a href="#">
+                  <span>Contact</span>
+                  <i>
+                    <svg class="msg-tel-icon" width="24" height="24" viewBox="0 0 24 24" fill="#fff">
+                      <use xlink:href="#msg-tel-icon"></use> 
+                    </svg>
+                  </i>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="hamburgar-cntlr show-lg">
+            <div class="hamburger-icon">
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</header>
+
+<div class="xs-mobile-menu">
+  <div class="xs-pop-up-menu-inr">
+    <div class="xs-pop-up-menu-top">
+      <div class="logo-close-btn-cntlr">
+        <div class="xs-logo">
+          <a href="front-page.html">
+            <img src="<?php echo THEME_URI; ?>/assets/images/logo.png" alt="">
+          </a>
+        </div>
+        <div class="hamburgar-cntlr show-lg">
+          <div class="hamburger-icon">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="xs-pop-menu-con">
+      <div class="xs-menu">
+        <nav class="main-nav">
+          <ul class="clearfix reset-list">
+            <li class="current-menu-item"><a href="#">Producten</a></li>
+            <li class="menu-item-has-children">
+              <a href="#">Verpakkingen</a>
+              <ul class="sub-menu">
+                <li><a href="#">submenu 1</a></li>
+                <li><a href="#">submenu 2</a></li>
+                <li><a href="#">submenu 3</a></li>
+              </ul>
+            </li>
+            <li><a href="#">Over ons</a></li>
+            <li><a href="#">Nieuws</a></li>
+            <li><a href="#">Vacatures</a></li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+    <div class="xs-pop-menu-btm">
+      <div class="hdr-lang-cntlr">
+        <div class="hdr-language">
+          <div class="fl-lang reset-slect">
+            <div class="fl-lang-cntlr">
+              <ul class="reset-list">
+                <li class="lang-active"><a href="#">Nl</a></li>
+                <li><a href="#">EN</a></li>
+                <li><a href="#">CN</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="hdr-contact-btn">
+        <a href="#">
+          <span>Contact</span>
+          <i>
+            <svg class="msg-tel-icon" width="24" height="24" viewBox="0 0 24 24" fill="#fff">
+              <use xlink:href="#msg-tel-icon"></use> 
+            </svg>
+          </i>
+        </a>
+      </div>
+    </div>
+  </div>
+</div>
