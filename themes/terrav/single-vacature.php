f771a30257123198be12ac6cdb216b8f700dff89
Azizul Haque <azizul.softsw@gmail.com>
2021-11-10 19:00:50 +0600

    Create single-vacature.php

---
+++ themes/terrav/single-vacature.php
@@ -0,0 +1,32 @@
+<?php
+get_header();
+?>
+<section class="page-banner">
+  <span class="pg-bnr-rgt-img hide-sm">
+    <img src="<?php echo THEME_URI; ?>/assets/images/page-bnr-right-img.svg" alt="">
+  </span>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="page-banner-cntlr">
+          <h1 class="fl-h1 page-banner-title"><?php the_title(); ?></h1>
+          <div class="breadcrumb-cntlr hide-sm">
+            <ul class="reset-list clearfix">
+              <li class="home">
+                <a href="<?php echo esc_url(home_url('/')); ?>">
+                  <span class="item"><?php _e('Home', 'terrav'); ?></span>
+                </a>
+              </li>
+              <li class="active">
+                <span><?php the_title(); ?></span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<?php get_template_part('templates/cta', 'sec'); ?>
+<?php get_footer(); ?>
